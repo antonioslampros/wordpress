58e6d0001fd144b5304d64ed97be7bf717fc9f66
antonioslampros <antonioslampros@gmail.com>
2023-06-04 17:59:20 +0300

    ytdfgdf

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,38 +1,31 @@
 import * as React from "react"
 import { graphql } from "gatsby"
-
 import Layout from "../components/layout"
 import * as sections from "../components/sections"
 import Fallback from "../components/fallback"
 import SEOHead from "../components/head"
+const isBrowser = typeof window !== "undefined"
 
 export default function Homepage(props) {
-  if (typeof window !== `undefined`) {
-    const { homepage } = props.data
+  let loggedIn = false
+  if (isBrowser) {
+    window.localStorage.getItem("isLoggedIn") === "true"
   }
+  const { homepage } = props.data
 
-  return (// Wrap the require in check for window
-  
+  return (
     <Layout>
       {homepage.blocks.map((block) => {
-        
-        if (typeof window !== `undefined`) {
-          const { id, blocktype, ...componentProps } = block
-          const Component = sections[blocktype] || Fallback
-          return <Component key={id} {...componentProps} />
-        }
-        
-        
-        
+        const { id, blocktype, ...componentProps } = block
+        const Component = sections[blocktype] || Fallback
+        return <Component key={id} {...componentProps} />
       })}
     </Layout>
   )
 }
 export const Head = (props) => {
-  if (typeof window !== `undefined`) {
-    const { homepage } = props.data
-  }
-
+  const { homepage } = props.data
+  
   return <SEOHead {...homepage} />
 }
 export const query = graphql`
